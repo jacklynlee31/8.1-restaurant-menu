583b5a1f1ceda4ff4bb628e026365a0b63f3d39f
Jacklyn Carroll <jacklynlee31@gmail.com>
2015-08-29 11:39:22 -0500

    Finished Styling

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -3,8 +3,11 @@ var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function(defaults) {
   var app = new EmberApp(defaults, {
-    // Add options here
+    emberCliFontAwesome: {
+      useScss: true
+    }
   });
+
 
   // Use `app.import` to add additional libraries to the generated
   // output files.
