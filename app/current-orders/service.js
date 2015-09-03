19c93ebd0e1a261607bdff14906c1701f2a1fd62
Jacklyn Carroll <jacklynlee31@gmail.com>
2015-09-03 08:55:32 -0500

    Working on getting the first level finished.

--- app/current-orders/service.js
+++ app/current-orders/service.js
@@ -2,29 +2,4 @@ import Ember from 'ember';
 
 export default Ember.Service.extend({
   orderedFood: [],
-
-  chooseFood: function(item) {
-    this.orderedFood.addObject(item);
-  },
-
-  markFood: function() {
-    // Grab all our checked in items
-    var saves = this.get('orderedFood').map((item)=> {
-      // Increment the orders property
-      item.incrementProperty('orders', 1);
-
-      //  *  Save the current item
-      //  *  and return it into the map
-
-      return item.save();
-    });
-
-    /**addOrder
-     * Wait for all items to finish saving
-     */
-    return Ember.RSVP.Promise.all(saves).then(() => {
-      // Empty out the orderedFood array
-      this.set('orderedFood', []);
-    });
-  }
 });
