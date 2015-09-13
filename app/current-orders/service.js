799ea74a5e7c1fec1e73b26127233e7ad975fb23
Jacklyn Carroll <jacklynlee31@gmail.com>
2015-09-13 16:06:02 -0500

    Working on getting the orders computed.

--- app/current-orders/service.js
+++ app/current-orders/service.js
@@ -2,4 +2,15 @@ import Ember from 'ember';
 
 export default Ember.Service.extend({
   orderedFood: [],
+
+  makeOrder: function(menuItem) {
+    this.orderedFood.pushObject({
+      menuItem: item,
+    });
+  },
+
 });
+
+// function that accepts a menuItem
+// pushes to orderedFood array
+// Ember.ArrayProxy@pushObject
