73c3d9ef39e2de28aaabdf5df1c144e63aae2758
Jacklyn Carroll <jacklynlee31@gmail.com>
2015-09-17 15:34:03 -0500

    Everything is all done! Totals are good to go.

--- app/current-orders/service.js
+++ app/current-orders/service.js
@@ -7,7 +7,15 @@ export default Ember.Service.extend({
     this.orderedFood.pushObject(menuItem);
   },
 
+  monies: Ember.computed.mapBy('orderedFood', 'price'),
+  price: Ember.computed.sum('monies')
+
 });
+
+// menuItem: Ember.computed.mapBy('price');
+
+  // Ember.computed.mapBy('menuItem.price');
+  // Ember.computed.sum('menuItem.price');
 
 // function that accepts a menuItem
 // pushes to orderedFood array
